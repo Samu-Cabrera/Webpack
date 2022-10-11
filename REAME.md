f94ac737ab843284967f118200a282d4c464e49c
Samu Cabrera <samucabreram99@gmail.com>
2022-10-11 19:09:54 -0300

    webpack settings

---
+++ REAME.md
@@ -0,0 +1,7 @@
+# Webpack 
+This is a configuration to start creating web applications with webpack
+
+## Proyect init
+- npm install
+- npm run build
+- npm run start
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizeWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'assets/js/main.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            emitFile: true,
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+          new CssMinimizeWebpackPlugin(),
+          new TerserWebpackPlugin()
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/styles/[name].css',
+            ignoreOrder: false
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { 
+                    from: 'src/assets/images', 
+                    to: 'assets/images/' 
+                }
+            ]
+        }),
+        new CleanWebpackPlugin(),
+    ]
+}
